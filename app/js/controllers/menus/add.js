1dd44a81334e82e4d1822e69f85e19b9b62b2680
richfab <fabien.richard@etu.univ-nantes.fr>
2014-10-23 11:18:44 -0400

    modal ajout menu

--- app/js/controllers/menus/add.js
+++ app/js/controllers/menus/add.js
@@ -5,5 +5,13 @@
 
 myApp.controller('MenuAddCtrl', ['$scope', '$modalInstance', 'Restangular',
     function ($scope, $modalInstance, Restangular) {
-
+        $scope.dishes = [{}];
+        
+        $scope.addDish = function () {
+            $scope.dishes.push({});
+        };
+        
+        $scope.save = function () {
+            console.log($scope.dishes);
+        };
     }]);
